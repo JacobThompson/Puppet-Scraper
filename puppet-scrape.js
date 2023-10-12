a9bad91cb6476f3a3ed270ffe5b6fa00ff3dcdcc
Jacob Thompson <jacobjake683@gmail.com>
2023-10-12 15:52:48 -0500

    Add page link URL discovery.

--- puppet-scrape.js
+++ puppet-scrape.js
@@ -2,6 +2,8 @@ const puppeteer = require("puppeteer");
 const fs = require('node:fs/promises');
 const options = require("./src/main/Options.js");
 const URLList = require("./src/main/URLList.js");
+const PUPPET_SCRAPE_LOG = require("./src/main/EventLog.js");
+const http = require("node:http");
 
 const readline = require('node:readline').createInterface({
 	input: process.stdin,
@@ -15,18 +17,29 @@ let args = new options(process.argv);
 	const browser = await puppeteer.launch({headless: 'new'});
 	const page = await browser.newPage();
 
-	const crawlQueue = new URLList([args.options.url]);
+	const crawlQueue = new URLList([args.options.url], args.options.url);
 
 	if(crawlQueue.getURLList.length == 0) {
 		console.log("Please enter a valid URL to scrape");	
 		process.exit();
 	}
 
+	if(args.options.sitemap) {
+			
+	}
+
 	for(const url of crawlQueue.getURLList) {
 		await page.goto(url);
-		const title = await page.title();
-		console.log(title);
+
+		if(args.options.pagediscover) {
+			const pageURLs = await page.evaluate(() => {
+				return Array.from(document.links).map(a => a.href);	
+			});
+
+			crawlQueue.addURLToList(pageURLs);
+		}
 	}
+	console.log(crawlQueue.getURLList);
 
 	await browser.close();
 	process.exit();
